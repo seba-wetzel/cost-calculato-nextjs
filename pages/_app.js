b0a432110174310a43e18a814a260a6f4541f884
seba-wetzel <seba.wetzel@gmail.com>
2022-08-17 23:23:14 -0300

    Add: simple login with google

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import {useInitializeFirebase} from 'firebase-hook-v9';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const initializeFirebase = useInitializeFirebase(
+    { 
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+    }
+  );
+  return initializeFirebase?  <Component {...pageProps} /> : null;
 }
 
 export default MyApp
